ad09408d68511c485c70683b8568c82e2e257f54
callMeCal <mtcalvert@Matthews-MacBook-Pro.local>
2017-11-29 19:39:45 -0500

    pep8 Improvements
    Shortened lines, corrected indents to bring to pep8 certified style.

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 class Movie(object):
-    '''Parent class for any movie instances. Stores trailer, title, and poster image data'''
+    '''Parent class for any movie instances.
+    Stores trailer, title, and poster image data'''
     def __init__(self, trailer_youtube_url, title, poster_image_url):
         self.trailer_youtube_url = trailer_youtube_url
         self.title = title
